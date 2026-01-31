7697b6b92421d5ce043aa6f2156a945a070ef0c3
Asya Sher <qa.sherstniuk@gmail.com>
2026-01-31 21:13:05 +0500

    Update Playwright configuration to increase timeout settings and adjust retry logic; ensure Prettier configuration ends with a newline.

--- playwright.config.js
+++ playwright.config.js
@@ -12,13 +12,17 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  timeout: 60_000,        // вместо 30s
+  expect: {
+    timeout: 10_000,
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
